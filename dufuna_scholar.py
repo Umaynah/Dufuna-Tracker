89c12f6bfb7de031ffc70bc2c3e7bbbbe2389725
Raji Humainat <rajihumainat@gmail.com>
2019-05-31 07:07:26 +0100

    Revert "Modified pages and flask files"
    This reverts commit 1c79440c71d5ce2cefd6e9b7b60ee993c6390675.

--- dufuna_scholar.py
+++ dufuna_scholar.py
@@ -1,1 +1,7 @@
-from app import app
+from flask import Flask,render_template
+app = Flask(__name__)
+@app.route("/")
+def main():
+	return render_template("home.html")
+if __name__ == '__main__':
+	app.run(port=5000, debug=True)
